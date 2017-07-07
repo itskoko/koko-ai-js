011c4dd5f17e9ad253643abd2f176156b81c4950
Jed Schmidt <where@jed.is>
2017-07-07 14:11:45 -0400

    first cut

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+const assert = require('assert')
 const https = require('https')
+const http = require('http')
+const url = require('url')
 
-module.exports = class Tracker {
+exports.Tracker = class Tracker {
   constructor(options) {
-    this.auth = options.auth
+    assert.ok('auth' in options, "options.auth not specified.")
+
+    const endpoint = options.endpoint || 'https://api.koko.ai'
+    const {protocol, host} = url.parse(endpoint)
+    const method = 'POST'
+    const headers = {
+      'Authorization': options.auth,
+      'Content-Type': 'application/json'
+    }
+
+    this.http = {http, https}[protocol.slice(0, -1)]
+    this.params = {method, host, headers}
   }
 
-  async trackContent(options) {
-    throw new Error('NOT IMPLEMENTED')
+  trackContent(options) {
+    return this.request('/track/content', options)
   }
 
-  async trackFlag(options) {
-    throw new Error('NOT IMPLEMENTED')
+  trackFlag(options) {
+    return this.request('/track/flag', options)
   }
 
-  async trackModeration(options) {
-    throw new Error('NOT IMPLEMENTED')
+  trackModeration(options) {
+    return this.request('/track/moderation', options)
+  }
+
+  request(path, options) {
+    return new Promise((resolve, reject) => {
+      const params = Object.assign({path}, this.params)
+
+      this.http.request(params)
+        .on('error', reject)
+        .on('response', onresponse)
+        .end(JSON.stringify(options))
+
+      function onresponse(response) {
+        let json = ''
+
+        response.setEncoding('utf8')
+        response.on('data', data => json += data)
+        response.on('end', () => {
+          try {
+            const data = JSON.parse(json)
+
+            'error' in data
+              ? reject(new Error(data.error[0]))
+              : resolve(data)
+          }
+
+          catch (e) {
+            const message = http.STATUS_CODES[response.statusCode]
+            reject(new Error(message))
+          }
+        })
+      }
+    })
   }
 }
---
+++ test.js
@@ -0,0 +1,38 @@
+process.on('unhandledRejection', err => { throw err })
+
+const {Tracker} = require('.')
+const koko = new Tracker({
+  auth: process.env.KOKO_AUTH
+})
+
+const responses = [
+  koko.trackContent({
+    id: '123',
+    user_id: '123',
+    type: 'post',
+    content_type: 'text',
+    content: {text: 'Some content'},
+    created_at: new Date(1498070225000).toISOString(),
+    context_id: '123'
+  }),
+
+  koko.trackFlag({
+    id: '123',
+    flagger_id: '123',
+    reasons: ['crisis'],
+    target: {content_id: "123"},
+    created_at: new Date(1498070225000).toISOString()
+  }),
+
+  koko.trackModeration({
+    id: '123',
+    moderator_id: '123',
+    action: 'user_warned',
+    target: {content_id: "123"},
+    reasons: ['abusive'],
+    created_at: new Date(1498070225000).toISOString(),
+    automated: false
+  })
+]
+
+Promise.all(responses)
