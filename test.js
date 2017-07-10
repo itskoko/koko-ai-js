f12d94eeb4890736492b6710fbeaf69aa7195914
Jed Schmidt <where@jed.is>
2017-07-10 14:42:53 -0400

    test select missing parameters

--- test.js
+++ test.js
@@ -1,36 +1,76 @@
+const assert = require('assert')
 const {Tracker} = require('.')
-const koko = new Tracker({
-  auth: process.env.KOKO_AUTH
-})
+const koko = new Tracker({auth: process.env.KOKO_AUTH})
 
-const responses = [
-  koko.trackContent({
+async function testTrackContent() {
+  const request = {
     id: '123',
     user_id: '123',
     type: 'post',
     content_type: 'text',
-    content: {text: 'Some content'},
-    created_at: new Date(1498070225000).toISOString(),
-    context_id: '123'
-  }),
+    content: {text: 'Some content'}
+  }
 
-  koko.trackFlag({
+  await koko.trackContent(request)
+
+  try {
+    delete request.type
+    await koko.trackContent(request)
+    throw new Error('API should require `type` parameter.')
+  }
+
+  catch ({message}) {
+    assert.equal(message, 'Required property type was not present.')
+  }
+}
+
+async function testTrackFlag() {
+  const request = {
     id: '123',
     flagger_id: '123',
     reasons: ['crisis'],
-    target: {content_id: "123"},
-    created_at: new Date(1498070225000).toISOString()
-  }),
+    targets: [{content_id: '123'}],
+    target: {content_id: '123'} // TODO: delete
+  }
 
-  koko.trackModeration({
+  await koko.trackFlag(request)
+
+  try {
+    delete request.flagger_id
+    await koko.trackFlag(request)
+    throw new Error('API should require `flagger_id` parameter.')
+  }
+
+  catch ({message}) {
+    assert.equal(message, 'Required property flagger_id was not present.')
+  }
+}
+
+async function testTrackModeration() {
+  const request = {
     id: '123',
     moderator_id: '123',
     action: 'user_warned',
-    target: {content_id: "123"},
-    reasons: ['abusive'],
-    created_at: new Date(1498070225000).toISOString(),
-    automated: false
-  })
+    target: {content_id: '123'}
+  }
+
+  await koko.trackModeration(request)
+
+  try {
+    delete request.moderator_id
+    await koko.trackModeration(request)
+    throw new Error('API should require `moderator_id` parameter.')
+  }
+
+  catch ({message}) {
+    assert.equal(message, 'Required property moderator_id was not present.')
+  }
+}
+
+const tests = [
+  testTrackContent(),
+  testTrackFlag(),
+  testTrackModeration()
 ]
 
-Promise.all(responses).catch(err => { throw err })
+Promise.all(tests).catch(err => { throw err })
